894652559cb414439c8eb4d2bfe6ad3608bfb64a
ka-kato <ka-kato@virtualex.local>
2019-05-16 11:46:49 +0900

    maven

--- src/tks/AccountResource.java
+++ src/tks/AccountResource.java
@@ -1,5 +1,48 @@
 package tks;
 
-public class AccountResource {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+
+import beans.Login;
+import dao.LoginDAO;
+
+@Path("accounts")
+public class AccountResource {
+	private final LoginDAO logDao = new LoginDAO();
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public boolean auth(final FormDataMultiPart form, @Context final HttpServletRequest request) throws WebApplicationException {
+		Login log = new Login();
+		log.setLoginId(form.getField("js-input-loginId").getValue());
+		log.setLoginPass(form.getField("js-input-loginPass").getValue());
+
+		HttpSession session = request.getSession(false);
+
+		if (session == null){
+		  /* まだ認証されていない */
+		  session = request.getSession(true);
+		}else{
+		  Object loginCheck = session.getAttribute("login");
+		  if (loginCheck == null){
+		    /* まだ認証されていない */
+		  }
+		}
+
+		boolean auth = logDao.auth(log);
+		if(auth==true){
+			session.setAttribute("loginId", log.getLoginId());
+			System.out.println(session.getAttribute("loginId"));
+		}
+		return logDao.auth(log);
+	}
 }
